4aba5922cacfc4d76df570cd968bb8e073728aad
nanos <nanos@thms.uk>
2023-04-03 09:21:25 +0100

    try to apply thread line patch to glitch

--- app/javascript/flavours/glitch/features/status/index.jsx
+++ app/javascript/flavours/glitch/features/status/index.jsx
@@ -57,6 +57,9 @@ import { textForScreenReader, defaultMediaVisibility } from 'flavours/glitch/com
 import Icon from 'flavours/glitch/components/icon';
 import { Helmet } from 'react-helmet';
 
+import { assignResponsesForStatus } from '.../../../javascript/mastodon/features/status/utils/assign_responses_for_status';
+import { setResponsesWithLinesData } from '.../../../javascript/mastodon/features/status/utils/set_lines_for_responses';
+
 const messages = defineMessages({
   deleteConfirm: { id: 'confirmations.delete.confirm', defaultMessage: 'Delete' },
   deleteMessage: { id: 'confirmations.delete.message', defaultMessage: 'Are you sure you want to delete this status?' },
@@ -125,7 +128,13 @@ const makeMapStateToProps = () => {
       });
     }
 
-    return Immutable.List(descendantsIds);
+    const statusesWithResponseHierarchy = assignResponsesForStatus(
+      statusId,
+      descendantsIds.map(id => statuses.get(id).toObject()),
+      3,
+    );
+
+    return Immutable.List(setResponsesWithLinesData( statusesWithResponseHierarchy ));
   });
 
   const mapStateToProps = (state, props) => {
@@ -571,6 +580,26 @@ class Status extends ImmutablePureComponent {
     ));
   }
 
+  renderDescendants (list) {
+    return list.map(({ id, children, lines }) => {
+      return (<div key={id} className={classNames('status__thread', { 'status__thread--last-item': lines?.lastChild, 'status__thread--first-item': lines?.firstChild })}>
+        <StatusContainer
+          key={`${id}${lines?.mode}`}
+          id={id}
+          onMoveUp={this.handleMoveUp}
+          onMoveDown={this.handleMoveDown}
+          contextType='thread'
+          lines={lines}
+        />
+        {children.length ? (
+          <>
+            {this.renderDescendants(children)}
+          </>
+        ) : null}
+      </div>);
+    });
+  }
+
   setExpansion = value => {
     this.setState({ isExpanded: value });
   };
@@ -634,7 +663,7 @@ class Status extends ImmutablePureComponent {
     }
 
     if (descendantsIds && descendantsIds.size > 0) {
-      descendants = <div>{this.renderChildren(descendantsIds)}</div>;
+      descendants = <div>{this.renderDescendants(descendantsIds)}</div>;
     }
 
     const isLocal = status.getIn(['account', 'acct'], '').indexOf('@') === -1;
