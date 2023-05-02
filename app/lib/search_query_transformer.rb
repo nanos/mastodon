b00d6f43c912355e839b77f2f9eaab27c6e541b6
nanos <nanos@thms.uk>
2023-05-02 20:54:17 +0100

    try to improve search
    https://universeodon.com/@vyr/110300500580681209

--- app/lib/search_query_transformer.rb
+++ app/lib/search_query_transformer.rb
@@ -159,7 +159,7 @@ class SearchQueryTransformer < Parslet::Transform
         when '-'
           :must_not
         when nil
-          :should
+          :must
         else
           raise Mastodon::SyntaxError, "Unknown operator: #{str}"
         end
